86d8a6b8b3982e086d154215d67ab8e3f50baa51
Victor Garcia Rubio <victorgrubiodl@gmail.com>
2021-06-21 20:56:00 +0200

    Created the security group

--- scripts/module_02/create-ec2-instance.js
+++ scripts/module_02/create-ec2-instance.js
@@ -1,11 +1,16 @@
 // Imports
 // TODO: Import the aws-sdk
+const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
 // TODO: Configure region
+AWS.config.update({
+  region='us-east-1'
+})
 
 // Declare local variables
 // TODO: Create an ec2 object
+const ec2 = new AWS.ec2()
 const sgName = 'hamster_sg'
 const keyName = 'hamster_key'
 
@@ -28,7 +33,51 @@ createSecurityGroup(sgName)
 // Create functions
 
 function createSecurityGroup (sgName) {
-  // TODO: Implement sg creation & setting SSH rule
+  let sg_name_params = {
+    Description: sgName,
+    GroupName: sgName
+  }
+
+  return new Promise((resolve, reject) => {
+    // creates security_group
+    ec2.createSecurityGroup(sg_name_params, (err, data) => {
+      if(err) reject(err)
+      else{
+        // SG rules: ssh and port 3000
+        // get id from previous call response
+        let sg_port_rules = {
+          GroupId: data.GroupId,
+          IpPermissions: [
+            {
+              IpProtocol: 'tcp',
+              FromPort: 22,
+              ToPort: 22,
+              IpRanges: [
+                {
+                  CidrIp: '0.0.0.0/0'
+                }
+              ]
+            },
+            {
+              IpProtocol: 'tcp',
+              FromPort: 3000,
+              ToPort: 3000,
+              IpRanges: [
+                {
+                  CidrIp: '0.0.0.0/0'
+                }
+              ]
+            }
+          ]
+        }
+        // applu rules to SG
+        ec2.authorizeSecurityGroup(sg_port_rules, (err) => {
+          if (err) reject(err)
+          else resolve()
+        })
+      }
+    })
+  })
 }
 
 function createKeyPair (keyName) {
